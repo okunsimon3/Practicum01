2c3d3cad55ea466b40efe97f1022e7fcc9f755d9
okunsimon3 <111754449+okunsimon3@users.noreply.github.com>
2024-09-02 19:04:50 -0400

    Part 3 & 4 completed

---
+++ src/ProductWriter.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class ProductWriter {
+    public static void main(String[] args) {
+        ArrayList<String> recs = new ArrayList<>();
+        Scanner in = new Scanner(System.in);
+        boolean doneInput = false;
+        String ID = "";
+        String name = "";
+        String description = "";
+        Double cost = 0.0;
+        String rec = "";
+        int yob = 0;
+
+
+
+
+        // uses a fixed known path:
+        //  Path file = Paths.get("c:\\My Documents\\data.txt");
+
+        // use the toolkit to get the current working directory of the IDE
+        // will create the file within the Netbeans project src folder
+        // (may need to adjust for other IDE)
+        // Not sure if the toolkit is thread safe...
+        File workingDirectory = new File(System.getProperty("user.dir"));
+        Path file = Paths.get(workingDirectory.getPath() + "\\src\\ProductTestData.txt");
+
+        /*
+        a.	ID (a String)
+        b.	Name
+        c.	Description
+        d.	Cost
+        */
+
+        do {
+            ID = SafeInput.getNonZeroLenString(in, "Enter your product ID 000001");
+            name = SafeInput.getNonZeroLenString(in, "Enter your product name");
+            description = SafeInput.getNonZeroLenString(in, "Enter your product description");
+            cost = SafeInput.getDouble(in, "Enter the cost of your product");
+
+            rec = ID + ", " + name + ", " + description + ", " + cost;
+
+            System.out.println(rec);
+
+            recs.add(rec);
+
+            doneInput = SafeInput.getYNConfirm(in, "Are you done?");
+
+        }while(!doneInput);
+
+
+        try
+        {
+            // Typical java pattern of inherited classes
+            // we wrap a BufferedWriter around a lower level BufferedOutputStream
+            OutputStream out =
+                    new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+            BufferedWriter writer =
+                    new BufferedWriter(new OutputStreamWriter(out));
+
+            // Finally can write the file LOL!
+
+            for(String r : recs)
+            {
+                writer.write(r, 0, r.length());  // stupid syntax for write rec
+                // 0 is where to start (1st char) the write
+                // rec. length() is how many chars to write (all)
+                writer.newLine();  // adds the new line
+
+            }
+            writer.close(); // must close the file to seal it and flush buffer
+            System.out.println("Data file written!");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
