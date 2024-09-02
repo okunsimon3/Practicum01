a31bb07176cd6acc4b0ab1041622ee19b4501a63
okunsimon3 <111754449+okunsimon3@users.noreply.github.com>
2024-09-02 18:13:19 -0400

    Part 1 & 2 started

---
+++ src/PersonGenerator.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class PersonGenerator {
+    public static void main(String[] args) {
+        boolean doneInput = false;
+        String ID = "";
+        String fname = "";
+        String lname = "";
+        String title = "";
+        int yob = 0;
+        String recs = "";
+
+
+
+        // uses a fixed known path:
+        //  Path file = Paths.get("c:\\My Documents\\data.txt");
+
+        // use the toolkit to get the current working directory of the IDE
+        // will create the file within the Netbeans project src folder
+        // (may need to adjust for other IDE)
+        // Not sure if the toolkit is thread safe...
+        File workingDirectory = new File(System.getProperty("user.dir"));
+        Path file = Paths.get(workingDirectory.getPath() + "\\src\\data.txt");
+
+        try
+        {
+            // Typical java pattern of inherited classes
+            // we wrap a BufferedWriter around a lower level BufferedOutputStream
+            OutputStream out =
+                    new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+            BufferedWriter writer =
+                    new BufferedWriter(new OutputStreamWriter(out));
+
+            // Finally can write the file LOL!
+
+            for(String rec : recs)
+            {
+                writer.write(rec, 0, rec.length());  // stupid syntax for write rec
+                // 0 is where to start (1st char) the write
+                // rec. length() is how many chars to write (all)
+                writer.newLine();  // adds the new line
+
+            }
+            writer.close(); // must close the file to seal it and flush buffer
+            System.out.println("Data file written!");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
